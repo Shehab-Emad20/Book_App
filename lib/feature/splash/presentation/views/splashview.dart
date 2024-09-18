91ab46f28bb57511b342c9a618ce36f779d81298
Shehab-Emad20 <shehabz2014@gmail.com>
2024-09-18 15:34:45 -0700

    Splash view Body

---
+++ lib/feature/splash/presentation/views/splashview.dart
@@ -0,0 +1,13 @@
+import 'package:bookly/feature/splash/presentation/views/widgets/splash_view_body.dart';
+import 'package:flutter/material.dart';
+
+class Splahview extends StatelessWidget {
+  const Splahview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}

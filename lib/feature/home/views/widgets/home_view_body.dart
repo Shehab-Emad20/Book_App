a5088f9a1bfeaf72a77f87154ed55e9e0af46073
Shehab-Emad20 <shehabz2014@gmail.com>
2024-09-18 20:43:02 -0700

    create custom app bar2

--- lib/feature/home/views/widgets/home_view_body.dart
+++ lib/feature/home/views/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:bookly/core/utils/assets.dart';
+import 'package:bookly/feature/home/views/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -8,27 +8,6 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Column(
       children: [CustomAppBar()],
-    );
-  }
-}
-
-class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
-      child: Row(
-        children: [
-          Image.asset(
-            AssetsData.logo,
-            height: 18,
-          ),
-          const Spacer(),
-          IconButton(onPressed: () {}, icon: const Icon(Icons.search, size: 24))
-        ],
-      ),
     );
   }
 }

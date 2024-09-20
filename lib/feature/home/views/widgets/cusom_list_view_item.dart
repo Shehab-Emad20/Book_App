22e8f676ec4bdfc42e667e7c62ba8be494c0f6cc
Shehab-Emad20 <shehabz2014@gmail.com>
2024-09-20 16:22:43 -0700

    Featured list view

--- lib/feature/home/views/widgets/cusom_list_view_item.dart
+++ lib/feature/home/views/widgets/cusom_list_view_item.dart
@@ -1,22 +1,19 @@
 import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class CusomListViewItem extends StatelessWidget {
-  const CusomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.3,
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Colors.red,
-              image: const DecorationImage(
-                  fit: BoxFit.fill, image: AssetImage(AssetsData.testImage))),
-        ),
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            color: Colors.red,
+            image: const DecorationImage(
+                fit: BoxFit.fill, image: AssetImage(AssetsData.testImage))),
       ),
     );
   }

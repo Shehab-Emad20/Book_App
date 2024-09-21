e6670b06e41da91ef96552a3c4b5a264d34c70b8
Shehab-Emad20 <shehabz2014@gmail.com>
2024-09-21 15:03:59 -0700

    Styles Part 1

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -3,4 +3,10 @@ import 'package:flutter/material.dart';
 abstract class Styles {
   static const titleMedium =
       TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
+
+  static const textStyle30 =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.w600);
+
+  static const textStyle18 =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
 }
